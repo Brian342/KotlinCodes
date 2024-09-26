a1b2299598789b0c47716d67b8130ce97d8ef7b8
Brian kimanzi <migelbrian3@gmail.com>
2024-09-26 06:23:46 +0300

    commit

--- helloKotlin.kt
+++ helloKotlin.kt
@@ -1,6 +1,24 @@
-fun main() {
-    var name: String = "Brian"
-    name = "Kimanzi"
+val number = 1234
+val name: String? = null // top level variables
+var greeting: String? = "Hello"
 
+fun main() {
+    // val is a read only keyword meaning it only assigned to one variable
+    // var can be assigned to another keyword
+//    name = "Kimanzi"
+    if(greeting != null){
+        println(greeting)
+
+    } else {
+        greeting = "hi"
+
+    }
+
+    println(name)
+
+    println(greeting)
     print(name)
+    when (greeting){
+
+    }
 }
