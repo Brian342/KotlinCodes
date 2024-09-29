c92b201a1f6eae5eea0717482b6a7fb2d007be80
Brian kimanzi <migelbrian3@gmail.com>
2024-09-29 22:49:46 +0300

    variables Kotlin code

---
+++ ByteShortIntLong.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var number: Int = 22
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("Int maximum value is: $maxIntegerValue")
+    println("Int minimum value is: $minIntegerValue")
+
+    val myMaxByteValue:Byte = Byte.MAX_VALUE
+    val myMinByteValue:Byte = Byte.MIN_VALUE
+
+    println("Byte Maximum Value: $myMaxByteValue!")
+    println("Byte Minimum Value: $myMinByteValue")
+
+    val myMaxShortValue: Short  = Short.MAX_VALUE
+    val myMinShortValue:Short = Short.MIN_VALUE
+
+    println("Short maximum Value is: $myMaxShortValue")
+    println("Short minimum Value is: $myMinShortValue")
+
+}
